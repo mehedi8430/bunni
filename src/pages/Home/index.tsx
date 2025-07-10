69bea7029fc939ce68e2b50e291b9bbb31e359ce
Niamulhuda-Nahid <niamulhuda6046@gmail.com>
2025-07-11 00:50:56 +0600

    feat: add FAQ component with accordion functionality and integrate Radix UI Accordion

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import HowDoesItWork from "./components/HowDoesItWork";
 import BusinessFeatures from "./components/BusinessFeatures";
 import StreamlineInvoicesTools from "./components/StreamlineInvoicesTools";
 import KeyBenefits from "./components/KeyBenefits";
+import FAQ from "./components/FAQ";
 
 export default function HomePage() {
   return <main>
@@ -11,5 +12,6 @@ export default function HomePage() {
     <BusinessFeatures />
     <StreamlineInvoicesTools />
     <KeyBenefits />
+    <FAQ />
   </main>;
 }
