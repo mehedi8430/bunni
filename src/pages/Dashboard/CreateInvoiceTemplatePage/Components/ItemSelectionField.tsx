9517c438f5d01f68c591c8d908f443cc7ff505fb
mehedi8430 <mehedialhasan1999@gmail.com>
2025-07-20 04:40:15 +0600

    implement combobox for auto complete

--- src/pages/Dashboard/CreateInvoiceTemplatePage/Components/ItemSelectionField.tsx
+++ src/pages/Dashboard/CreateInvoiceTemplatePage/Components/ItemSelectionField.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import { useForm, Controller } from "react-hook-form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-} from "@/components/ui/select";
 import { ReactSVG } from "react-svg";
 import { Input } from "@/components/ui/input";
 import { useProductApi } from "@/mock-api-hook/features/customers/useProductsApi";
 import assets from "@/lib/imageProvider";
-import { useState, useMemo } from "react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Check } from "lucide-react";
 
 interface ItemSelectionFieldProps {
   itemId: string;
@@ -30,20 +42,10 @@ export default function ItemSelectionField({
       itemDescription: "",
     },
   });
-  const [isSelectOpen, setIsSelectOpen] = useState(false);
-
-  const itemDescription = watch("itemDescription");
-
-  // Filter products based on search input
-  const filteredProducts = useMemo(() => {
-    if (!itemDescription) return products;
-    return products.filter((product) =>
-      product.name.toLowerCase().includes(itemDescription.toLowerCase()),
-    );
-  }, [itemDescription, products]);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleInputClick = () => {
-    setIsSelectOpen(true);
+    setIsOpen(true);
   };
 
   const handleSelectChange = (value: string) => {
@@ -52,41 +54,67 @@ export default function ItemSelectionField({
       setValue("itemDescription", selectedProduct.name);
       onItemSelect(itemId, selectedProduct.name);
     }
-    setIsSelectOpen(false);
+    setIsOpen(false);
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsSelectOpen(true);
     setValue("itemDescription", e.target.value);
     onItemSelect(itemId, e.target.value);
+    setIsOpen(true);
   };
+
+  const itemDescription = watch("itemDescription");
 
   return (
     <div className="flex items-center gap-2">
-      <Select
-        open={isSelectOpen}
-        onOpenChange={setIsSelectOpen}
-        onValueChange={handleSelectChange}
-      >
-        <SelectTrigger className="data-[placeholder]:text-foreground data-[select-trigger]:text-foreground w-[20px] border-none bg-transparent shadow-none focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 data-[placeholder]:text-lg data-[select-trigger]:text-lg [&>svg]:hidden">
-          <button type="button" className="h-8 w-8" onClick={handleInputClick}>
+      <Popover open={isOpen} onOpenChange={setIsOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={isOpen}
+            className="border-none bg-transparent p-0 shadow-none focus:ring-0 focus:ring-offset-0"
+          >
             <ReactSVG src={assets.icons.addIcon} />
-          </button>
-        </SelectTrigger>
-        <SelectContent>
-          {filteredProducts.length > 0 ? (
-            filteredProducts.map((product) => (
-              <SelectItem key={product.id} value={product.name}>
-                {product.name}
-              </SelectItem>
-            ))
-          ) : (
-            <div className="px-4 py-2 text-sm text-gray-500">
-              No products found
-            </div>
-          )}
-        </SelectContent>
-      </Select>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0" align="start">
+          <Command>
+            <CommandInput
+              placeholder="Search items..."
+              className="text-foreground/90 h-9"
+              value={itemDescription}
+              onValueChange={(value) => {
+                setValue("itemDescription", value);
+                onItemSelect(itemId, value);
+              }}
+            />
+            <CommandList>
+              <CommandEmpty>No item found.</CommandEmpty>
+              <CommandGroup>
+                {products.map((product) => (
+                  <CommandItem
+                    key={product.id}
+                    value={product.name}
+                    onSelect={handleSelectChange}
+                    className="text-muted-foreground data-[selected=true]:text-muted-foreground"
+                  >
+                    {product.name}
+                    <Check
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        itemDescription === product.name
+                          ? "opacity-100"
+                          : "opacity-0",
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
 
       <Controller
         name="itemDescription"
@@ -101,7 +129,7 @@ export default function ItemSelectionField({
               handleInputChange(e);
             }}
             onClick={handleInputClick}
-            className="text-muted-foreground border-0 text-[16px] shadow-none focus:ring-0 focus:ring-offset-0 focus:outline-none focus-visible:ring-0"
+            className="text-muted-foreground -ml-2 border-0 text-[16px] shadow-none focus:ring-0 focus:ring-offset-0 focus:outline-none focus-visible:ring-0"
             value={itemDescription}
           />
         )}
@@ -109,100 +137,3 @@ export default function ItemSelectionField({
     </div>
   );
 }
-
-// import { useForm, Controller } from "react-hook-form";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-// } from "@/components/ui/select";
-// import { ReactSVG } from "react-svg";
-// import { Input } from "@/components/ui/input";
-// import { useProductApi } from "@/mock-api-hook/features/customers/useProductsApi";
-// import assets from "@/lib/imageProvider";
-// import { useState } from "react";
-
-// interface ItemSelectionFieldProps {
-//   itemId: string;
-//   onItemSelect: (itemId: string, description: string) => void;
-// }
-
-// interface FormData {
-//   itemDescription: string;
-// }
-
-// export default function ItemSelectionField({
-//   itemId,
-//   onItemSelect,
-// }: ItemSelectionFieldProps) {
-//   const { products } = useProductApi();
-//   const { control, setValue, watch } = useForm<FormData>({
-//     defaultValues: {
-//       itemDescription: "",
-//     },
-//   });
-//   const [isSelectOpen, setIsSelectOpen] = useState(false);
-
-//   const handleInputClick = () => {
-//     setIsSelectOpen(true);
-//   };
-
-//   const handleSelectChange = (value: string) => {
-//     const selectedProduct = products.find((product) => product.name === value);
-//     if (selectedProduct) {
-//       setValue("itemDescription", selectedProduct.name);
-//       onItemSelect(itemId, selectedProduct.name);
-//     }
-//     setIsSelectOpen(false);
-//   };
-
-//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setIsSelectOpen(true);
-//     onItemSelect(itemId, e.target.value);
-//   };
-
-//   const itemDescription = watch("itemDescription");
-
-//   return (
-//     <div className="flex items-center gap-2">
-//       <Select
-//         open={isSelectOpen}
-//         onOpenChange={setIsSelectOpen}
-//         onValueChange={handleSelectChange}
-//       >
-//         <SelectTrigger className="data-[placeholder]:text-foreground data-[select-trigger]:text-foreground w-[20px] border-none bg-transparent shadow-none focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 data-[placeholder]:text-lg data-[select-trigger]:text-lg [&>svg]:hidden">
-//           <button type="button" className="h-8 w-8" onClick={handleInputClick}>
-//             <ReactSVG src={assets.icons.addIcon} />
-//           </button>
-//         </SelectTrigger>
-//         <SelectContent>
-//           {products.map((product) => (
-//             <SelectItem key={product.id} value={product.name}>
-//               {product.name}
-//             </SelectItem>
-//           ))}
-//         </SelectContent>
-//       </Select>
-
-//       <Controller
-//         name="itemDescription"
-//         control={control}
-//         render={({ field }) => (
-//           <Input
-//             {...field}
-//             type="text"
-//             placeholder="Type or click to select an item..."
-//             onChange={(e) => {
-//               field.onChange(e);
-//               handleInputChange(e);
-//             }}
-//             onClick={handleInputClick}
-//             className="text-muted-foreground border-0 text-[16px] shadow-none focus:ring-0 focus:ring-offset-0 focus:outline-none focus-visible:ring-0"
-//             value={itemDescription}
-//           />
-//         )}
-//       />
-//     </div>
-//   );
-// }
