0f3d5c51f0bb2a2d5d1c9b4c29a3ad6f01817228
Niamulhuda-Nahid <niamulhuda6046@gmail.com>
2025-07-24 04:12:13 +0600

    feat: enhance internationalization by adding key benefits section and updating translations in English and Spanish

--- src/pages/Home/components/KeyBenefits.tsx
+++ src/pages/Home/components/KeyBenefits.tsx
@@ -1,33 +1,35 @@
 import SectionTitle from "@/components/shared/SectionTitle";
 import HowDoesItWorkCard from "./HowDoesItWorkCard";
 import { icons } from "@/lib/imageProvider";
+import { useTranslation } from "react-i18next";
 
 
 export default function KeyBenefits() {
+    const { t } = useTranslation();
     return (
         <section className="container mx-auto mb-10 md:mb-12 lg:mb-16 px-5">
-           <SectionTitle title=" Key Benefits Section" subtitle="Stop wasting time on paperwork. Create and send professional invoices or estimates in just a few clicks with customizable templates." className="w-full lg:w-[68%] mx-auto" />
+           <SectionTitle title={t("key_benefits_title")} subtitle={t("key_benefits_subtitle")} className="w-full lg:w-[68%] mx-auto" />
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                 <HowDoesItWorkCard
                     icon={<img src={icons.file} alt="Document Icon" className="w-10 h-10 md:w-14 md:h-14" />}
-                    title="Create & Send Invoices Fast"
-                    description="Generate professional invoices or estimates in seconds — send via email, text, or link."
+                    title={t("key_benefit_1_title")}
+                    description={t("key_benefit_1_description")}
                     classNameForIcon="flex justify-center"
                     className="text-center p-10"
                     classNameForDescription="text-description"
                 />
                 <HowDoesItWorkCard
                     icon={<img src={icons.requring} alt="Document Icon" className="w-10 h-10 md:w-14 md:h-14" />}
-                    title="Recurring Billing"
-                    description="Set up auto-invoices with flexible schedules for subscriptions or repeat clients."
+                    title={t("key_benefit_2_title")}
+                    description={t("key_benefit_2_description")}
                     classNameForIcon="flex justify-center"
                     className="text-center p-10"
                     classNameForDescription="text-description"
                 />
                 <HowDoesItWorkCard
                     icon={<img src={icons.reportingFilter} alt="Document Icon" className="w-10 h-10 md:w-14 md:h-14" />}
-                    title=" Smart Reporting Filters"
-                    description="Track status, payment type, and invoice history with real-time filters."
+                    title= {t("key_benefit_3_title")}
+                    description={t("key_benefit_3_description")}
                     classNameForIcon="flex justify-center"
                     className="text-center p-10"
                     classNameForDescription="text-description"
