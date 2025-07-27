2e4074118634b1731741bc60782fde698f48f663
Niamulhuda-Nahid <niamulhuda6046@gmail.com>
2025-07-27 06:34:13 +0600

    feat: replace DownloadGamma with DownloadBeta in invoice viewer; update styles and structure in DownloadBeta component

--- src/pages/Dashboard/components/ForView.tsx
+++ src/pages/Dashboard/components/ForView.tsx
@@ -42,7 +42,8 @@ export default function ForView() {
   return (
     <div>
         <PDFViewer width={1280} height={720}>
-          <DownloadGamma invoice={transformedInvoiceData} />
+          <DownloadBeta invoice={transformedInvoiceData} />
+          {/* <DownloadGamma invoice={transformedInvoiceData} /> */}
         </PDFViewer>
     </div>
   )
